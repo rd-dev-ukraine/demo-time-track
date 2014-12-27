c7d5aba6901430f0072e6f2bf4a3f948a5bd865b
SergeyTokarev <seregat1984@gmail.com>
2014-12-27 14:05:31 +0200

    Initial commit

---
+++ LanceTrack.Domain/Projects/Project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LanceTrack.Domain.Projects
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime? EndTime { get; set; }
+    }
+}
---
+++ LanceTrack.Domain/TimeTracking/ITimeTrackingService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace LanceTrack.Domain.TimeTracking
+{
+    public interface ITimeTrackingService
+    {
+        void TrackTime(int projectId, DateTime time, string description);
+    }
+}
