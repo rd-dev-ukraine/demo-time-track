83a13398645efeb376dcbd61378667d461e7a74a
SergeyTokarev <seregat1984@gmail.com>
2014-12-27 14:49:58 +0200

    Two projects added.

--- LanceTrack.Domain/UserAccounts/IUserAccountRepository.cs
+++ LanceTrack.Domain/UserAccounts/IUserAccountRepository.cs
@@ -3,7 +3,5 @@
     public interface IUserAccountRepository
     {
         UserAccount Login(string email, string password);
-
-        void CheckUserAuthorizedForProject(int userId, int projectId);
     }
 }
